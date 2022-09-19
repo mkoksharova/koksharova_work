45b77b50f1489d26ac197f6539ff36ac9266164d
mkoksharova <110388813+mkoksharova@users.noreply.github.com>
2022-09-19 12:38:59 +0300

    final code

--- example/Program.cs
+++ example/Program.cs
@@ -19,3 +19,25 @@ void PrintArray(string[] array)
     Console.Write("]");
     Console.WriteLine();
 }
+void ThreeSymbolArray(string[] array)
+{
+    string[] stringarray = new string[array.Length];
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            stringarray[i] = array[i];
+            Console.Write($"{stringarray[i]}");
+            if (i != array.Length - 1)
+                Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+string[] array = { "hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan", "GG", "11", "(_)" };
+PrintArray(array);
+Console.WriteLine();
+ThreeSymbolArray(array);
