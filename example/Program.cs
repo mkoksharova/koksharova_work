e4721280bfe9150399e8fb1e354b60e05151d13f
mkoksharova <110388813+mkoksharova@users.noreply.github.com>
2022-09-19 12:38:40 +0300

    start coding

--- example/Program.cs
+++ example/Program.cs
@@ -6,3 +6,16 @@
 // ["1234", "1567", "-2", "computer science"] -> ["-2"]
 // ["Russia", "Denmark", "-2", "Kazan"] -> []
 
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (var i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+
+        if (i != array.Length - 1)
+            Console.Write(", ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
